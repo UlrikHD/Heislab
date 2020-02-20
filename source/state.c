4655cf64a914be983a847f8fccb1a5ed1bde82e7
Kristian Nilsen <krnil@stud.ntnu.no>
2020-02-20 18:49:40 +0100

    changes

--- source/state.c
+++ source/state.c
@@ -4,131 +4,112 @@
 
 
 State g_state = IDLE;
-State state_getState() 
-{
+State state_getState() {
 	return g_state;
 }
 
-void state_setState(State state) 
-{
+void state_setState(State state) {
 	g_state = state;
-
 	return;
 }
 
-int state_getLastFloor()
-{
+int state_getLastFloor(){
 	return g_lastFloor;
 }
 
-int state_getDirection()
-{
+int state_getDirection(){
 	return g_direction;
 }
 
-int state_getAtFloor()
-{
+int state_getAtFloor(){
 	return g_atFloor;
 }
 
-void state_setDirection(int floor)
-{
+void state_setDirection(int floor){
 	int lastFloor = state_getLastFloor();
-	if (floor != lastFloor) 
-	{
-		if (floor < lastFloor)
-		{
+	if (floor != lastFloor) {
+		if (floor < lastFloor){
 			state_setState(MOVING_DOWN);
 			//state_stateSwitch();
 		}
-		else if (floor > lastFloor)
-		{
+		else if (floor > lastFloor){
 			state_setState(MOVING_UP);
 			//state_stateSwitch();
 		}
 	}
-	else if (floor == lastFloor)
-	{
+	else if (floor == lastFloor){
 		bool lastDirection = state_getDirection();
-		if (lastDirection) //up
-		{
+		if (lastDirection) {//up
 			state_setState(MOVING_DOWN);
 			//state_stateSwitch();
 		}
-		else if (!lastDirection) //down
-		{
+		else if (!lastDirection) {//down
 			state_setState(MOVING_UP);
 			//state_stateSwitch();
 		}
 	}
 }
 
-void state_startTimer()
-{
+void state_startTimer(){
 	g_timer = clock();
 }
 
-int state_timerDone(int seconds)
-{
+int state_timerDone(int seconds){
 	clock_t currentTime = clock();
 	double timeElapsed = difftime(currentTime, g_timer);
 	
-	if (timeElapsed < seconds)
-	{
+	if (timeElapsed < seconds){
 		return 0;
 	}
-	else if (timeElapsed >= seconds)
-	{
+	else if (timeElapsed >= seconds){
 		return 1; //timer done
 	}
 	return 0;
 }
 
-void state_setAtFloor(int value)
-{
-	if(value == 1 || value == 0)
-	{
+void state_setAtFloor(int value){
+	if(value == 1 || value == 0){
 		g_atFloor = value;
 	}
 	return;
 }
 
-void state_setLastFloor(int floor)
-{
-	if(0 < floor || floor < 5)
-	{
+void state_setLastFloor(int floor){
+	if(0 < floor || floor < 5){
 		g_lastFloor = floor;
 	}
 	return;
 }
 
 
-void state_stateSwitch()
-{
+void state_stateSwitch(){
 		
-	switch (state_getState())
-	{
-		case IDLE: 
-		{
+	switch (state_getState()){
+		case IDLE: {
+			printf("idle\n");
+			hardware_command_door_open(0);
 			hardware_command_movement(HARDWARE_MOVEMENT_STOP);
 			break;
 		}
-		case MOVING_UP: 
-		{
+		case MOVING_UP: {
+			state_setAtFloor(0);
+			printf("moving up\n");
+			hardware_command_door_open(0);
 			hardware_command_movement(HARDWARE_MOVEMENT_UP);
 			break;
 		}
-		case MOVING_DOWN: 
-		{
+		case MOVING_DOWN: {
+			state_setAtFloor(0);
+			printf("moving down\n");
+			hardware_command_door_open(0);
 			hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
 			break;
 		}
-		case AT_FLOOR: 
-		{
+		case AT_FLOOR: {
+			printf("at floor\n");
 			hardware_command_movement(HARDWARE_MOVEMENT_STOP);
 			int sensorRead = 0;
-			for (int i = 1; i<5;i++)
-			{
+			for (int i = 1; i<5;i++){
 				if(hardware_read_floor_sensor(i))
 				{
 					sensorRead = 1;
@@ -152,8 +133,16 @@ void state_stateSwitch()
 			break;
 		}
 		default:
-		case STOP: 
-		{
+		case STOP: {
+			hardware_command_movement(HARDWARE_MOVEMENT_STOP);
+			hardware_command_stop_light(1);
+			if(state_getAtFloor()){
+				hardware_command_door_open(1);
+			}
+			state_startTimer();
+			//flush orders
+			//dont take orders?
+
 			break;
 		}
 	}
--- source/state.h
+++ source/state.h
@@ -7,6 +7,7 @@
 */
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <time.h>
 
 #ifndef STATE_H
